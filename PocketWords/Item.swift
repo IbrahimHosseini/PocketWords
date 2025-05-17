d0f1f1f28c6e5dac5a06a187b792d115c7d13407
Ibrahim Hosseini <sehosseini@me.com>
2025-05-17 13:40:01 +0330

    Initial Commit

---
+++ PocketWords/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PocketWords
+//
+//  Created by Ibrahim  on 5/17/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PocketWords/PocketWordsApp.swift
@@ -0,0 +1,32 @@
+//
+//  PocketWordsApp.swift
+//  PocketWords
+//
+//  Created by Ibrahim  on 5/17/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PocketWordsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
